a34eafd6793257128059adc5851767384f9fc4f6
Grimstir <xbio.oo@gmail.com>
2019-04-30 21:14:38 +0800

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  #maps to the home action in the controller
   get 'static_pages/home'
   get 'static_pages/help'
-  root "application#hello"
+  #this automatically creates a helper called static_pages_about_url
+  get 'static_pages/about'
+  get 'static_pages/contact_us'
 end
